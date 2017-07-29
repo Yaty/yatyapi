0e56a84c7727bd926bb49daec9ee08c475a996e3
Yaty <yaty@live.fr>
2017-07-30 00:19:47 +0200

    New structure for JWT usage

--- config.js
+++ config.js
@@ -2,5 +2,13 @@ module.exports = {
     port: 8081,
     staticPath: __dirname + '/static',
     mongodb: 'mongodb://localhost:27017/yaty',
-    secret: 'a]4@SjZed*FU>L-9563e'
+    token: {
+        secret: 'a]4@SjZed*FU>L-9563e',
+        options: {
+            expiresIn: 50400,
+            audience: 'http://localhost:8090/',
+            issuer: 'http://localhost:8081/'
+        }
+    },
+    loggerLevel: 'debug'
 };
