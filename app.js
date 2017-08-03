6959c2e35d2986031f9ce2943f639154f19d6906
Yaty <yaty@live.fr>
2017-08-04 00:02:53 +0200

    Wait for all mongoose models to be ready to launch the application Using CRONS from the new file by iterating to launch them Gyms route Logging

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
+/*
+Copyright (C) Hugo Da Roit <contact@hdaroit.fr> - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Hugo Da Roit <contact@hdaroit.fr>, 2017
+Based on Vue-admin from Fangdun Cai <cfddream@gmail.com>
+*/
+
 const config = require('./config');
 const logger = require('./modules/logger');
-const cron = require('cron');
 const express = require('express');
+const routers = require('./routers');
 const app = express();
+const crons = require('./modules/crons');
 
 // Launch Mongoose
-require('./modules/mongodb/connection')();
+require('./modules/mongodb/connection')()
+    .then(() => {
+        // Enabling CORS
+        app.use(require('cors')());
 
-// CORS when we are in dev
-app.use(require('cors')());
+        // JSON Body-parser
+        app.use(require('body-parser').json());
 
-// JSON Body-parser
-app.use(require('body-parser').json());
+        // Logging HTTP request
+        app.use(require('morgan')("combined", { stream: { write: message => logger.info(message.trim()) }}));
 
-// Logging HTTP request
-app.use(require('morgan')("combined", { stream: { write: message => logger.info(message.trim()) }}));
+        // Routes
+        app.use('/static', express.static(config.staticPath));
+        app.use('/auth', routers.auth);
+        app.use('/gyms', routers.gyms);
 
-// Routes
-app.use('/static', express.static(config.staticPath));
-app.use('/auth', require('./routers/auth'));
+        // Error handler
+        app.use(require('./modules/middlewares').errorHandler);
 
-// Error handler
-app.use(require('./modules/middlewares').errorHandler);
+        // Listening
+        app.listen(config.port, () => {
+            logger.info(`Server launched on ${config.port}`);
+        });
 
-// Listening
-app.listen(config.port, () => {
-    logger.info(`Server launched on ${config.port}`);
-});
+        // Logging unhandled rejected promises
+        process.on('unhandledRejection', (reason) => {
+            logger.warn('Unhandled promise rejection', { reason });
+        });
 
-// Logging unhandled rejected promises
-process.on('unhandledRejection', (reason) => {
-    logger.warn('Unhandled promise rejection', { reason });
-});
-
-// Crons
-const mongoStatsJob = new cron.CronJob({
-    cronTime: '00 00 00 * * *', // Run everyday at 00h00
-    onTick() {
-        console.log('Making mongodb stats');
-    }
-});
-
-const mongoBackupJob = new cron.CronJob({
-    cronTime: '00 00 03 * * *', // Run everyday at 03h00
-    onTick() {
-        console.log('Making mongodb backup');
-    }
-});
-
-mongoStatsJob.start();
-mongoBackupJob.start();
+        // Starting CRON jobs
+        Object.keys(crons).forEach(job => crons[job].start());
+    })
+    .catch((e) => {
+        const error = require('./modules/errors').CustomError(e);
+        logger.error('Error while running the app.', { error })
+    });
