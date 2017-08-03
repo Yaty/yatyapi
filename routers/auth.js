1c1e6af0e6979a8ad5abc93befd28ae8f2b0d3a1
Yaty <yaty@live.fr>
2017-08-03 23:55:58 +0200

    Refactor

--- routers/auth.js
+++ routers/auth.js
@@ -1,85 +1,49 @@
-const express = require('express');
-const router = express.Router();
+/*
+Copyright (C) Hugo Da Roit <contact@hdaroit.fr> - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Hugo Da Roit <contact@hdaroit.fr>, 2017
+Based on Vue-admin from Fangdun Cai <cfddream@gmail.com>
+*/
+
+const router =require('express').Router();
 const mongodb = require('../modules/mongodb').users;
-const logger = require('../modules/logger');
-const config = require('../config');
 const authMiddlewares = require('../modules/middlewares').auth;
 const JWTCheck = authMiddlewares.checkJWT;
 const JWTSign = authMiddlewares.signJWT;
+const CustomError = require('../modules/errors').CustomError;
 
 router.post('/register', (req, res, next) => {
     mongodb.register(req.body.name, req.body.lastname, req.body.email, req.body.password)
-        .then(() => {
-            logger.debug('Registration successful', { name: req.body.name, lastname: req.body.lastname, email: req.body.email });
-            return res.sendStatus(201);
-        })
-        .catch((e) => {
-            logger.info('Registration unsuccessful', { name: req.body.name, lastname: req.body.lastname, email: req.body.email, error: e });
-            return next(e);
-        });
+        .then(() => res.sendStatus(201))
+        .catch((e) => next(new CustomError(e, "POST /register failure :  " + JSON.stringify({ name: req.body.name, lastname: req.body.lastname, email: req.body.email }))));
 });
 
 router.post('/login', JWTSign, (req, res, next) => {
     mongodb.login(req.body.email, req.body.password)
         .then((user) => {
-            if (user === null) {
-                logger.debug('Login unsuccessful', { email: req.body.email });
-                return res.sendStatus(401);
-            } else if (user.isMatch) {
-                const token = res.locals.token; // set by the auth middleware
-                return res.json({ token });
-            } else {
-                throw "Unknown error while login.";
-            }
+            if (user === null) throw new CustomError(CustomError.TYPES.AUTH_ERRORS.BAD_USER);
+            else if (user.isMatch) return res.json({ token: res.locals.token });
+            else throw new CustomError(CustomError.TYPES.AUTH_ERRORS.BAD_PASSWORD);
         })
-        .catch((e) => {
-            logger.info('Error while login', { email: req.body.email, error: e });
-            return next(e);
-        });
+        .catch((e) => next(new CustomError(e, "POST /login failure : " + req.body.email)));
 });
 
 router.post('/logout', JWTCheck, (req, res, next) => {
     mongodb.removeTokenFromUserByEmail(res.locals.email)
-        .then(() => {
-            return res.sendStatus(200);
-        })
-        .catch((e) => {
-            logger.info('Error while logout', { email: res.locals.email, error: e });
-            return next(e);
-        });
+        .then(() => res.sendStatus(200))
+        .catch((e) => next(new CustomError(e, "POST /logout failure : " + res.locals.email)));
 });
 
 router.get('/refresh', JWTCheck, (req, res, next) => {
     // TODO : create a new token and return it
-    return res.status(200).json({
-        status: 'success'
-    });
+    return res.sendStatus(200);
 });
 
 router.get('/user', JWTCheck, (req, res, next) => {
-    // TODO
     mongodb.getUserInfoByEmail(res.locals.email)
-        .then(user => {
-            return res.json({
-                data: user
-            });
-        })
-        .catch(e => {
-            logger.info('Error while getting user', { email: res.locals.email, error: e });
-            return next(e);
-        });
+        .then((user) => res.json({ data: user }))
+        .catch((e) => next(new CustomError(e, "GET /user failure : " + res.locals.email)));
 });
-
-/*
-router.post('/jwt/verify', (req, res, next) => {
-    const token = req.body.token;
-    jwt.verify(token, config.secret, (err, decode) => {
-       if (err) return next(err);
-       return res.json({
-          validity: typeof decode !== 'undefined'
-       });
-    });
-});
-*/
 
 module.exports = router;
